8b4cde248df0a9e44e812a04b028bb1a59562f36
maswiecho <maswiecho@gmail.com>
2024-07-17 20:18:31 +0200

    0.1

---
+++ ckeditor-collaboration/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development", // or 'production'
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+};
